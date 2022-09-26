2c1b7998816e4cbf8a35a770203b6dd0ed7a2fa2
cyhsieh0119 <96895088+cyhsieh0119@users.noreply.github.com>
2022-09-26 12:16:57 +0800

    Update uitls.py

--- uitls.py
+++ uitls.py
@@ -1,11 +1,15 @@
 import pandas as pd
 
-cols = pd.MultiIndex.from_tuples(['No', 'Notes', '刮刀下壓', 'Panel ID', '出貨', '洗淨方式', '破片', '可剝膠',
-                                   DB, VDD, Vbc, Vss2, VGL, VGH, 
-								   Date, VDD, Vbc, Vss2, VGL, VGH, 
-								   VDD, Vbc, Vss2, VGL, VGH, 
-								   VDD, Vbc, Vss2, VGL, VGH, 
-								   VDD, Vbc, Vss2, VGL, VGH,
+lists = [['N9 切後', '刷鍑前', '刷鍑後', 'COB 後'], ['VDD', 'Vbc', 'Vss2', 'VGL', 'VGH']]
+
+index = pd.MultiIndex.from_product(lists, names=['first', 'second'])
+
+cols = pd.MultiIndex.from_tuples(['No', 'Notes', '刮刀下壓', 'Panel ID', '出貨', '洗淨方式', '破片', '可剝膠', 'DB',
+                                   'VDD', 'Vbc', 'Vss2', 'VGL', 'VGH', 
+				  				   'VDD', 'Vbc', 'Vss2', 'VGL', 'VGH', 
+								   'VDD', 'Vbc', 'Vss2', 'VGL', 'VGH',  
+								   'VDD', 'Vbc', 'Vss2', 'VGL', 'VGH', 
+								   'VDD', 'Vbc', 'Vss2', 'VGL', 'VGH', 
 
 								  #("Gasoline", "Toyoto"), 
 								  #("Gasoline", "Ford"), 	
