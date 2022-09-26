8de061c7746bc2f3dae292dd2e364b16e4cf7288
cyhsieh0119 <96895088+cyhsieh0119@users.noreply.github.com>
2022-09-26 11:09:07 +0800

    Create uitls.py

---
+++ uitls.py
@@ -0,0 +1,14 @@
+import pandas as pd
+
+cols = pd.MultiIndex.from_tuples(['No', 'Notes', '刮刀下壓', 'Panel ID', '出貨', '洗淨方式', '破片', '可剝膠',
+                                   DB, VDD, Vbc, Vss2, VGL, VGH, 
+								   Date, VDD, Vbc, Vss2, VGL, VGH, 
+								   VDD, Vbc, Vss2, VGL, VGH, 
+								   VDD, Vbc, Vss2, VGL, VGH, 
+								   VDD, Vbc, Vss2, VGL, VGH,
+
+								  #("Gasoline", "Toyoto"), 
+								  #("Gasoline", "Ford"), 	
+								  #("Electric", "Tesla"),
+								  #("Electric", "Nio")   	
+								  ])
