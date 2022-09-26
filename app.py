5c74a7a56a177cf69f0d9c33ce75e637cb9c049d
cyhsieh0119 <96895088+cyhsieh0119@users.noreply.github.com>
2022-09-26 18:27:10 +0800

    Update app.py

--- app.py
+++ app.py
@@ -6,22 +6,7 @@ from st_aggrid import GridOptionsBuilder, AgGrid, GridUpdateMode, DataReturnMode
 
 df = pd.read_parquet('beta.parquet.gzip') 
 
-grid_options = {
-    "columnDefs": [
-        {
-            "headerName": "col1,
-            "field": "col1",
-            "editable": True,
-        },
-        {
-            "headerName": "col2",
-            "field": "col2",
-            "editable": False,
-        },
-    ],
-}
-
-grid_return = AgGrid(df, grid_options)
+grid_return = AgGrid(df, editable=True)
 new_df = grid_return["data"]
 
 st.write(new_df)
