6711a8821a3055da44b1dae223b3b63587e2be6a
cyhsieh0119 <96895088+cyhsieh0119@users.noreply.github.com>
2022-09-26 19:04:41 +0800

    Update app.py

--- app.py
+++ app.py
@@ -5,9 +5,9 @@ import numpy as np
 from st_aggrid import GridOptionsBuilder, AgGrid, GridUpdateMode, DataReturnMode, JsCode
 
 df = pd.read_parquet('beta.parquet.gzip') 
-
+df1 = pd.read_csv('beta.csv')
 #new_df = 
 #AgGrid(df)#, editable=True)
 #new_df = grid_return["data"]
-st.write(df)
+st.write(df1)
 
