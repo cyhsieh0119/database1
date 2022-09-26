2785fa056288626a30c4c1c60ca57863bfc8e94b
cyhsieh0119 <96895088+cyhsieh0119@users.noreply.github.com>
2022-09-26 18:14:50 +0800

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import plotly.express as px
 import numpy as np
-
+from st_aggrid import GridOptionsBuilder, AgGrid, GridUpdateMode, DataReturnMode, JsCode
 
 df = pd.read_parquet('beta.parquet.gzip') 
 
